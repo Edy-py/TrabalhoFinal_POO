27259e53928e6615da71c41b2b38a355c6078918
Edy-py <edilsonalvesprofissional@gmail.com>
2025-08-11 16:15:25 -0300

    a

--- BancoDeDados/src/main/java/ConexaoBanco.java
+++ BancoDeDados/src/main/java/ConexaoBanco.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConexaoBanco {
+
+    public static Connection conectar() throws IOException, SQLException {
+        Properties props = new Properties();
+
+        // Lê o db.properties de src/main/resources
+        try (InputStream input = ConexaoBanco.class.getResourceAsStream("/db.properties")) {
+            if (input == null) {
+                throw new IOException("Arquivo db.properties não encontrado em resources!");
+            }
+            props.load(input);
+        }
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
 }
--- BancoDeDados/src/main/java/Jogos.java
+++ BancoDeDados/src/main/java/Jogos.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
-
 public class Jogos {
+
+    private String nome,dev,console,esrb;
+    private int ano, qtd, diasAlugados,qtdDisponivel;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDev() {
+        return dev;
+    }
+    public void setDev(String dev) {
+        this.dev = dev;
+    }
+    public String getConsole() {
+        return console;
+    }
+    public void setConsole(String console) {
+        this.console = console;
+    }
+    public int getAno() {
+        return ano;
+    }
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    public int getQtd() {
+        return qtd;
+    }
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+    public int getDiasAlugados() {
+        return diasAlugados;
+    }
+    public void setDiasAlugados(int diasAlugados) {
+        this.diasAlugados = diasAlugados;
+    }
+    public int getQtdDisponivel() {
+        return qtdDisponivel;
+    }
+    public void setQtdDisponivel(int qtdDisponivel) {
+        this.qtdDisponivel = qtdDisponivel;
+    }
+    public String getEsrb() {
+        return esrb;
+    }
+    public void setEsrb(String esrb) {
+        this.esrb = esrb;
+    }
+
+
+
 }
